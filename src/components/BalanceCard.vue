542bd74d30b0d36c014c7c450ca15cec33e35a45
theelgreco <stelios.kakoliris@yahoo.com>
2025-08-22 23:17:31 +0100

    deploy

--- src/components/BalanceCard.vue
+++ src/components/BalanceCard.vue
@@ -43,7 +43,6 @@ const animateCounter = (targetValue: number) => {
 
 // Easing function for smooth animation (quadratic ease-in-out)
 const easeInOutQuad = (t: number) => {
-  // return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
   return t
 };
 
